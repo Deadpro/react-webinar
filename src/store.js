0a37d301b5906ff47183eb1186997aa2928b5eb8
Che Vladimir <iamcomputers@mail.ru>
2023-05-12 16:27:55 -0400

    test

--- src/store.js
+++ src/store.js
@@ -23,32 +23,35 @@ class Store {
     }
 
     /**
-     * Выбор состояния
+     * Выбор состояния. Если запись по ключу "code" отсутствует в сессионном хранилище, то создается такая запись, куда помещается длина списка минус единица. Будет правильно работать только в том случае, если длина первичного списока минус единица сповпадает с максимальным кодом в списке.
      * @returns {Object}
      */
     getState() {
+        if (sessionStorage.getItem("code") === null)
+            sessionStorage.setItem("code", parseInt(this.state.list[this.state.list.length - 1]['code']));
         return this.state;
     }
     
     /**
-     * Установка состояния
+     * Установка состояния. Если существующий код в сессионном хранилище меньше нового когда, то новый код записывается в сессионное хранилище.
      * @param newState {Object} 
      */
     setState(newState) {
+        if (parseInt(sessionStorage.getItem('code')) < newState.list[newState.list.length - 1]['code'])
+            sessionStorage.setItem("code", parseInt(newState.list[newState.list.length - 1]['code']));
         this.state = newState;
-        sessionStorage.setItem("nnumbers", parseInt(this.state.list[this.state.list.length - 1]['code']));
-        // sessionStorage.removeItem("lastNumber");
+
         // Вызываем всех слушателей
         for (const listener of this.listeners) listener();
     }    
 
     /**
-     * Добавление новой записи
+     * Добавление новой записи. Код для новой записи берется из сессионного хранилища и прибавляется 1, т.е. новая запись больше на 1.
      */
     addItem() {
         this.setState({
             ...this.state,
-            list: [...this.state.list, {code: parseInt(sessionStorage.getItem("nnumbers")) + parseInt(1), title: 'Новая запись'}]
+            list: [...this.state.list, {code: parseInt(sessionStorage.getItem("code")) + parseInt(1), title: 'Новая запись'}]
         })
     };
 
@@ -68,11 +71,29 @@ class Store {
      * @param code
      */
     selectItem(code) {
-        this.setState({
+        if (sessionStorage.getItem(code) === null) {
+            sessionStorage.setItem(code, 1);
+        } else {
+            sessionStorage.setItem(code, parseInt(sessionStorage.getItem(code)) + 1);
+        }
+
+        let selectionCounterText = " | Выделяли " + sessionStorage.getItem(code) + " раз";
+            
+        this.setState({            
             ...this.state,
             list: this.state.list.map(item => {
                 if (item.code === code) {
                     item.selected = !item.selected;
+                    let insertText = "";
+                    if (item.title.search("Выделяли") > 0) {
+                        insertText = item.title.substring(0, item.title.search("Выделяли")-2) + selectionCounterText;
+                    } else {
+                        insertText = item.title + selectionCounterText;
+                    }
+                    item.title = insertText;
+                } else {
+                    if (item.selected === true)
+                        item.selected = !item.selected;
                 }
                 return item;
             })
